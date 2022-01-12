e77735664a5443be0b9a72666b6d983f6ac803f5
zhangyunpeng <zhangyunpeng@sensorsdata.cn>
2022-01-12 11:41:50 +0800

    remove others

--- packages/electron-umi-template/src/main/config/config.js
+++
@@ -1,5 +0,0 @@
-/**
- * 主进程全局配置
- */
-
-export const DEV_ADDRESS = 'http://localhost:9090'
--- packages/electron-umi-template/src/render/utils/polyfill/electron-store.js
+++
@@ -1,8 +0,0 @@
-module.exports = class Store {
-  get(name) {
-    return localStorage.getItem(name)
-  }
-  set(name, val) {
-    localStorage.setItem(name, val)
-  }
-}
